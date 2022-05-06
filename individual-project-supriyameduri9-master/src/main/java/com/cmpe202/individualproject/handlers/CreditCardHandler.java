d9ec6deafb291e80f8f06c0b5dacdf4d79340506
rp440 <ruchitpatel440@gmail.com>
2022-05-05 21:56:54 -0700

    Removed unnecessary files

--- individual-project-supriyameduri9-master/src/main/java/com/cmpe202/individualproject/handlers/CreditCardHandler.java
+++
@@ -1,7 +0,0 @@
-package com.cmpe202.individualproject.handlers;
-
-import com.cmpe202.individualproject.main.CreditCardEntry;
-
-public interface CreditCardHandler {
-    public String checkCreditCardType(CreditCardEntry creditCardEntry);
-}
